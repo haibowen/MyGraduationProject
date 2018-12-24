7975961216be6384676acbcfd79b23de82714edb
haibowen <1461154748@qq.com>
2018-12-24 15:41:55 +0800

    加入了轮播图

--- FileManagementAssistant/app/src/main/java/com/example/administrator/filemanagementassistant/fragment/FindFragment.java
+++ FileManagementAssistant/app/src/main/java/com/example/administrator/filemanagementassistant/fragment/FindFragment.java
@@ -6,11 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.bumptech.glide.ListPreloader;
 import com.example.administrator.filemanagementassistant.R;
+import com.example.administrator.filemanagementassistant.util.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindFragment extends Fragment {
     private View mview;
+    @BindView(R.id.banner)
+    public Banner banner;
+    private List<Integer>images=new ArrayList<>();
+
 
     @Nullable
     @Override
@@ -18,6 +31,16 @@ public class FindFragment extends Fragment {
         mview = inflater.inflate(R.layout.find_fragment, null);
 
         ButterKnife.bind(this, mview);
+        images.add(R.drawable.first);
+        images.add(R.drawable.second);
+        images.add(R.drawable.third);
+        banner  .setImages( images)
+
+                .setDelayTime( 4000 )
+                .isAutoPlay( true )
+                .setBannerAnimation( Transformer.Tablet )
+                .setImageLoader( new GlideImageLoader() ).start();
+
         return mview;
 
 
