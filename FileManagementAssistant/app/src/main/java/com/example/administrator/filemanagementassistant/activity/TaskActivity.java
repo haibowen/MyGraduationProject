6eaa0960d438822f03655269cac86332b8afe9f8
haibowen <1461154748@qq.com>
2019-02-18 16:30:19 +0800

    修改了分享界面

--- FileManagementAssistant/app/src/main/java/com/example/administrator/filemanagementassistant/activity/TaskActivity.java
+++ FileManagementAssistant/app/src/main/java/com/example/administrator/filemanagementassistant/activity/TaskActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -32,12 +33,17 @@ import com.example.administrator.filemanagementassistant.adapter.MyDirAdapter;
 import com.example.administrator.filemanagementassistant.bean.DirFile;
 import com.example.administrator.filemanagementassistant.util.MarqueeTextView;
 import com.example.administrator.filemanagementassistant.util.SdCardUtil;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.vincent.filepicker.Constant;
 import com.vincent.filepicker.activity.AudioPickActivity;
 import com.vincent.filepicker.activity.ImagePickActivity;
 import com.vincent.filepicker.activity.NormalFilePickActivity;
 import com.vincent.filepicker.activity.VideoPickActivity;
+import com.vincent.filepicker.filter.entity.AudioFile;
+import com.vincent.filepicker.filter.entity.ImageFile;
+import com.vincent.filepicker.filter.entity.NormalFile;
+import com.vincent.filepicker.filter.entity.VideoFile;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -49,7 +55,7 @@ import java.util.List;
 import static com.vincent.filepicker.activity.AudioPickActivity.IS_NEED_RECORDER;
 import static com.vincent.filepicker.activity.ImagePickActivity.IS_NEED_CAMERA;
 
-public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,View.OnClickListener {
 
     private ViewPager viewPager;
 
@@ -64,6 +70,9 @@ public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageC
     private RecyclerView recyclerView;
     private FloatingActionsMenu floatingActionsMenu;
     private SwipeRefreshLayout swipeRefreshLayout;
+
+    private FloatingActionButton floatingActionButton1,floatingActionButton2,floatingActionButton3,floatingActionButton4;
+
 
 
     private View view;
@@ -141,8 +150,21 @@ public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageC
 
 
         floatingActionsMenu = view.findViewById(R.id.bt_fabMenu);
-        mview = view.findViewById(R.id.display);
+        floatingActionButton1=view.findViewById(R.id.bt_image);
+        floatingActionButton2=view.findViewById(R.id.bt_audio);
+        floatingActionButton3=view.findViewById(R.id.bt_file);
+        floatingActionButton4=view.findViewById(R.id.bt_video);
 
+
+        floatingActionButton1.setOnClickListener(this);
+        floatingActionButton2.setOnClickListener(this);
+        floatingActionButton3.setOnClickListener(this);
+        floatingActionButton4.setOnClickListener(this);
+
+
+
+
+        mview = view.findViewById(R.id.display);
         textView_path = view.findViewById(R.id.textpath);
         Intent intent=getIntent();
         path= intent.getStringExtra("path");
@@ -205,10 +227,6 @@ public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageC
 
 
           //  Dirlist= (List<DirFile>) getIntent().getSerializableExtra("mylist");
-
-
-
-
 
 
 
@@ -631,6 +649,142 @@ public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     }
 
+
+    //wenhaibo add 20190218 回调
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        switch (requestCode) {
+
+            case Constant.REQUEST_CODE_PICK_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<ImageFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_IMAGE);
+
+                    ArrayList<Uri> utilist = new ArrayList<>();
+                    List<String> mylist = new ArrayList<>();
+
+                    for (ImageFile file : list) {
+                        String path = file.getPath();
+                        mylist.add(path);
+
+                    }
+                    for (int i = 0; i < mylist.size(); i++) {
+                        utilist.add(Uri.fromFile(new File(mylist.get(i))));
+                    }
+                    Intent shareIntent = new Intent();
+                    //打开分享界面的Action
+                    shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                    //分享传递的数据
+                    shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, utilist);
+                    //指定分享的类型
+                    shareIntent.setType("image/*");
+                    //打开分享
+                    startActivity(Intent.createChooser(shareIntent, "分享到"));
+
+
+                }
+
+
+                break;
+            case Constant.REQUEST_CODE_PICK_VIDEO:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<VideoFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_VIDEO);
+
+                    ArrayList<Uri> utilist = new ArrayList<>();
+                    List<String> mylist = new ArrayList<>();
+
+                    for (VideoFile file : list) {
+                        String path = file.getPath();
+                        mylist.add(path);
+
+                    }
+                    for (int i = 0; i < mylist.size(); i++) {
+                        utilist.add(Uri.fromFile(new File(mylist.get(i))));
+                    }
+                    Intent shareIntent = new Intent();
+                    //打开分享界面的Action
+                    shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                    //分享传递的数据
+                    shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, utilist);
+                    //指定分享的类型
+                    shareIntent.setType("video/*");
+                    //打开分享
+                    startActivity(Intent.createChooser(shareIntent, "分享到"));
+
+
+                }
+                break;
+            case Constant.REQUEST_CODE_PICK_AUDIO:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<AudioFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_AUDIO);
+                    ArrayList<Uri> utilist = new ArrayList<>();
+                    List<String> mylist = new ArrayList<>();
+
+                    for (AudioFile file : list) {
+                        String path = file.getPath();
+                        mylist.add(path);
+
+                    }
+                    for (int i = 0; i < mylist.size(); i++) {
+                        utilist.add(Uri.fromFile(new File(mylist.get(i))));
+                    }
+
+                    Intent shareIntent = new Intent();
+                    //打开分享界面的Action
+                    shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                    //分享传递的数据
+                    shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, utilist);
+                    //指定分享的类型
+                    shareIntent.setType("audio/*");
+                    //打开分享
+                    startActivity(Intent.createChooser(shareIntent, "分享到"));
+
+
+                }
+                break;
+            case Constant.REQUEST_CODE_PICK_FILE:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<NormalFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_FILE);
+                    ArrayList<Uri> utilist = new ArrayList<>();
+                    List<String> mylist = new ArrayList<>();
+
+                    for (NormalFile file : list) {
+                        String path = file.getPath();
+                        mylist.add(path);
+
+                    }
+                    for (int i = 0; i < mylist.size(); i++) {
+                        utilist.add(Uri.fromFile(new File(mylist.get(i))));
+                    }
+
+                    Intent shareIntent = new Intent();
+                    //打开分享界面的Action
+                    shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                    //分享传递的数据
+                    shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, utilist);
+                    //指定分享的类型
+                    shareIntent.setType("text/*");
+                    //打开分享
+                    startActivity(Intent.createChooser(shareIntent, "分享到"));
+
+                }
+                break;
+        }
+
+    }
+
+
+    //wenhaibo add 20190218 分享的回调
+
+
+
+
+
+
     //文件夹以及文件的搜索
     public List<DirFile> searchDir(File flist) {
         //final File[] fileDir;
@@ -731,4 +885,45 @@ public class TaskActivity extends AppCompatActivity implements ViewPager.OnPageC
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.bt_image:
+
+                Intent intent1 = new Intent(TaskActivity.this, ImagePickActivity.class);
+                intent1.putExtra(IS_NEED_CAMERA, true);
+                intent1.putExtra(Constant.MAX_NUMBER, 9);
+                startActivityForResult(intent1, Constant.REQUEST_CODE_PICK_IMAGE);
+
+                break;
+
+            case R.id.bt_audio:
+                Intent intent4 = new Intent(TaskActivity.this, NormalFilePickActivity.class);
+                intent4.putExtra(Constant.MAX_NUMBER, 9);
+                intent4.putExtra(NormalFilePickActivity.SUFFIX, new String[]{"xlsx", "xls", "doc", "docx", "ppt", "pptx", "pdf"});
+                startActivityForResult(intent4, Constant.REQUEST_CODE_PICK_FILE);
+
+
+                break;
+
+            case R.id.bt_file:
+                Intent intent3 = new Intent(TaskActivity.this, AudioPickActivity.class);
+                intent3.putExtra(IS_NEED_RECORDER, true);
+                intent3.putExtra(Constant.MAX_NUMBER, 9);
+                startActivityForResult(intent3, Constant.REQUEST_CODE_PICK_AUDIO);
+
+                break;
+
+            case R.id.bt_video:
+                Intent intent2 = new Intent(TaskActivity.this, VideoPickActivity.class);
+                intent2.putExtra(IS_NEED_CAMERA, true);
+                intent2.putExtra(Constant.MAX_NUMBER, 9);
+                startActivityForResult(intent2, Constant.REQUEST_CODE_PICK_VIDEO);
+                break;
+
+        }
+
+    }
 }
